88a69d770690ecb6fdf56dc97c142634b58951e7
TANAKA Masayuki <tanakamasayuki@gmail.com>
2026-05-29 18:40:52 +0900

    feat: Enhance consumer task synchronization with release flag

--- tests/runtime/freertos_notify/freertos_notify.ino
+++ tests/runtime/freertos_notify/freertos_notify.ino
@@ -8,10 +8,14 @@
 
 static TaskHandle_t g_consumer = nullptr;
 static std::atomic<uint32_t> g_received{0};
+static std::atomic<bool> g_release{false};
 static std::atomic<bool> g_done{false};
 
 static void consumer(void *arg) {
     (void)arg;
+    while (!g_release.load()) {
+        vTaskDelay(1);
+    }
     const uint32_t v = ulTaskNotifyTake(pdTRUE, pdMS_TO_TICKS(2000));
     g_received.store(v);
     g_done.store(true);
@@ -23,11 +27,11 @@ void setup() {
     Serial.println("TEST start freertos_notify");
 
     xTaskCreate(consumer, "consumer", 4096, nullptr, 1, &g_consumer);
-    delay(20);
 
     xTaskNotifyGive(g_consumer);
     xTaskNotifyGive(g_consumer);
     xTaskNotifyGive(g_consumer);
+    g_release.store(true);
 
     const uint32_t t0 = millis();
     while (!g_done.load() && (millis() - t0) < 2000) {
